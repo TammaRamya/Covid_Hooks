f780c92fc8fb228a83cbe08c465d704586d7e57d
TammaRamya <84433894+TammaRamya@users.noreply.github.com>
2021-06-29 14:26:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import DataFetching from './Components/FetchingData'
+
+
+function App() {
+  return (
+    <div>
+      <DataFetching />
+    </div>
+  );
+}
+export default App;
---
+++ src/Components/Table2.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import 'antd/dist/antd.css';
+import { Table} from 'antd';
+function Tabledis(props){
+
+    const columns = [
+      {
+        title: 'Country',
+        dataIndex:'Country',
+        key: 'Country',
+        sorter: (a, b) => a.Country.length - b.Country.length,
+      },
+      {
+        title: 'Date',
+        dataIndex: 'Date',
+        key: 'Date',
+      },
+      {
+        title: 'Confirmed',
+        dataIndex: 'NewConfirmed',
+        key: 'NewConfirmed',
+        sorter: (a, b) => a.NewConfirmed - b.NewConfirmed,
+      },
+      {
+        title: 'Recovered',
+        dataIndex: 'NewRecovered',
+        key: 'NewRecovered',
+        sorter: (a, b) => a.NewRecovered - b.NewRecovered,
+      },
+      {
+        title: 'Deaths',
+        dataIndex: 'NewDeaths',
+        key: 'NewDeaths',
+        sorter: (a, b) => a.NewDeaths - b.NewDeaths,
+      },
+    ];
+    
+    const data = props.datafromApi
+        console.log(data)
+    return(
+        <div>
+    <Table  columns={columns} dataSource={data} /></div>
+    )
+}
+export default Tabledis
